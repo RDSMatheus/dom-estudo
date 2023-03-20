4895046edaee2ea9f4b11bfa7ecdb22f327cdecd
Matheus <matheus_rds1998@hotmail.com>
2023-03-20 20:11:48 -0300

    adicionado debounce na animação scroll

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,4 +1,4 @@
-import outsideClick from './outsideclick.js';
+import outsideClick from './outside-click.js';
 
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
