6b424353b370b3f9593ff2fab3368e4403189891
Matheus <matheus_rds1998@hotmail.com>
2023-03-22 00:55:01 -0300

    adicionado slide fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from './modules/menu-mobile.js';
 import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import SlideNav from "./modules/slide.js";
 
 const scroll = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scroll.init();
@@ -41,7 +42,6 @@ fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin('https://blockchain.info/ticker', ".btc-preco")
 
-
-
-
-
+const slide = new SlideNav(".slide", ".wrapper");
+slide.init();
+slide.addControl(".custom-controls");
